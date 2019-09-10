593060106a61a8b751055fec8fd9d31560b31894
abhinav ashok <abhinav.ashok@tivo.com>
2019-09-10 14:44:57 +0530

    commit 1

--- gitreverrtmerge/src/main/java/Main.java
+++ gitreverrtmerge/src/main/java/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("commit1");
+        method1();
+    }
+
+    public static void method1(){
+        System.out.println("method : commit1");
+    }
 }
