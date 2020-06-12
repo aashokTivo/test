3c5ebe97f5164ab00309f08bfbc218120eb651a7
aashok <abhinav.ashok@tivo.com>
2020-06-12 18:13:48 +0530

    c1

--- gitreverrtmerge/src/main/java/Main.java
+++ gitreverrtmerge/src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("commit1");
-        method1();
+        method2();
     }
 
-    public static void method1(){
+    public static void method2(){
         System.out.println("method : commit1");
     }
 }
